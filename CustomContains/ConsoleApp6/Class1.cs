041f851c285b595ff9295a7e81c7a69f92492477
Sabir M. Guliyev <sabirmg@code.local>
2022-03-01 11:38:29 +0400

    Classwork_added

---
+++ CustomContains/ConsoleApp6/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    static class Class1
+    {
+        public static bool Cn(this string a, string w)
+        {
+            bool result = false;
+            StringBuilder st = new StringBuilder();
+            int j = 0;
+            int count = 0;
+            for (int i = 0; i < a.Length;i++ )
+            {
+                
+                if (w[j] == a[i])
+                {
+                    st.Append(a[i]);
+                    j++;
+                   
+                    count++;
+                  
+                    if (st.ToString().ToLower()==w.ToLower())
+                    {
+                        result = true;
+                        break;
+                    }                                            
+                                           
+                }
+                else
+                {
+                    if (j!=0)
+                    {
+                        count++;
+                        count -= st.Length;
+                        
+                        i = i - count;
+                        st.Clear();
+                        j = 0;
+                    }
+                    else
+                    {
+                        
+                    }
+                    
+                    
+
+
+                   
+                }
+                
+                
+
+                
+            }
+
+
+
+            return result;
+        }
+    }
+}
---
+++ CustomContains/ConsoleApp6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "azerbaycan";
+            string w = "az";
+            Console.WriteLine(a.Cn(w));
+        }
+        
+    }
+}
