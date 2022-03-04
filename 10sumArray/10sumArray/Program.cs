616b34ffb7f64a80f91f6cca776f5f509809218c
Sabir M. Guliyev <sabirmg@code.local>
2022-03-04 12:21:09 +0400

    TargetSumArray_done

---
+++ 10sumArray/10sumArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _10sumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 0, 2,3, 4, 5, 6, 7, 9, 10 };
+            
+
+            foreach (int numbers in SumTargetArr(num,9))
+            {
+                Console.WriteLine(numbers);
+            }
+        }
+        public static int [] SumTargetArr(int [] arr,int num)
+        {
+            int[] SumofTarget = new int[0];
+            int f = 0;
+
+            for (int i = 1; f < arr.Length - 1; i++)
+            {
+                if (i < arr.Length)
+                {
+                    if (arr[f] + arr[i] == num)
+                    {
+                        Array.Resize(ref SumofTarget, SumofTarget.Length + 1);
+                        SumofTarget[SumofTarget.Length - 1] = arr[f];
+                        Array.Resize(ref SumofTarget, SumofTarget.Length + 1);
+                        SumofTarget[SumofTarget.Length - 1] = arr[i];
+                    }
+                }
+                else
+                {
+                    i = f + 1;
+                    f++;
+                }
+            }
+            return SumofTarget;
+        }
+    }
+}
