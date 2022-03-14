21bbd654e90eeeff612639be96113402919c476c
Sabir M. Guliyev <sabirmg@code.local>
2022-03-14 14:15:17 +0400

    UserLogin_started

---
+++ UserLogin/UserLogin/LoginService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserLogin
+{
+    class LoginService
+    {
+        List<User> _users = new List<User>();
+        List<User> Users => _users;
+        public void Register()
+        {
+
+        }
+        public string CheckName()
+        {            
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine().Trim().ToLower();
+            do
+            {
+                Console.WriteLine("Please enter correct value");
+                name = Console.ReadLine().Trim().ToLower();
+
+            } while (name.Length < 3 || CheckLetters(name) == false);
+            return Capitalized(name);
+
+        }
+        public string CheckSurname()
+        {
+            Console.WriteLine("Please enter your surname");
+            string surname = Console.ReadLine().Trim().ToLower();
+            do
+            {
+                Console.WriteLine("Please enter correct value");
+                surname = Console.ReadLine().Trim().ToLower();               
+
+            } while (surname.Length<3||CheckLetters(surname)==false);            
+            return Capitalized(surname);
+        }
+        public string CheckUsername()
+        {
+            string username = Console.ReadLine().Trim();
+            
+
+        }
+        public string CheckPassword()
+        {
+            string password =Console.ReadLine();
+        }
+        public byte CheckAge()
+        {
+            Console.WriteLine("Please enter your age");
+            byte age= 0;
+            string str = (Console.ReadLine());
+            bool result = byte.TryParse(str, out age);
+            do
+            {
+                Console.WriteLine("Please enter correct value");
+                str = (Console.ReadLine());
+                result = byte.TryParse(str, out age);
+            } while (!result||age<7&&age>200);
+            return age;
+        }
+        public string Capitalized(string item)
+        {
+            StringBuilder str = new StringBuilder();
+            str = str.Append(Char.ToUpper(item[0]));
+            for (int i = 1; i < item.Length; i++)
+            {
+                str = str.Append(item[i]);
+
+            }
+            return str.ToString();
+        }
+        public bool CheckLetters(string item)
+        {
+            bool result = true;
+            for (int i = 0; i < item.Length; i++)
+            {
+                if (!char.IsLetter(item[i]))
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+       
+    }
+}
---
+++ UserLogin/UserLogin/User.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserLogin
+{
+    class User
+    {
+        private string _username;
+        private string _password;
+        private string _name;
+        private string _surname;
+        private byte _age;
+        public string Username
+        { get 
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                _surname = value;
+            }
+        }
+        public byte Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+
+        public User(string name,string surname,string username,string password,byte age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Username = username;
+            Password = password;
+        }
+    }
+}
